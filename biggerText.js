2d1f35fcdd5e3dea1d5f2a71e4df8a7000fbb5dd
EvMilev <92635038+EvMilev@users.noreply.github.com>
2022-06-06 18:22:25 +0200

    DOM first try

---
+++ biggerText.js
@@ -0,0 +1,8 @@
+let bigger = document.getElementById('circle-name');
+
+function biggerCircle () {
+    bigger.style.width = '40rem'
+    bigger.style.height = '40rem'
+};
+
+bigger.addEventListener('mouseover', biggerCircle);

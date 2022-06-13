20e11ab210c484678e13465d2ef186019d55556d
EvMilev <92635038+EvMilev@users.noreply.github.com>
2022-06-13 19:31:42 +0200

    DOM bigger and smaller circle

--- biggerText.js
+++ biggerText.js
@@ -1,8 +1,26 @@
-let bigger = document.getElementById('circle-name');
+
+let targetCircle = document.querySelector('.circle-name');
+let targetFontSize = document.querySelector('.name-and-text')
 
 function biggerCircle () {
-    bigger.style.width = '40rem'
-    bigger.style.height = '40rem'
+    targetCircle.style.width = '40rem';
+    targetCircle.style.height = '40rem';
+    targetFontSize.style.fontSize = '1.1rem';
 };
 
-bigger.addEventListener('mouseover', biggerCircle);
+function smallerCircle () {
+    targetCircle.style.width = '35rem'
+    targetCircle.style.height = '35rem'
+    targetFontSize.style.fontSize = '1rem'
+};
+
+
+// targetCircle.onmouseover = function() {biggerCircle()}
+// targetCircle.onmouseout = function() {smallerCircle()}
+
+
+targetCircle.addEventListener('mouseover', biggerCircle);
+targetCircle.addEventListener('mouseout', smallerCircle);
+
+
+
